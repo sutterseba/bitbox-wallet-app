929568e761f3a95d44259d97103537953f42e13e
Sebastian Sutter <mail@sutterseba.de>
2024-11-17 11:27:40 +0100

    ci: build windows app
    Adds a new job to GitHub actions CI to build the Windows app.
    
    WIP

--- frontends/qt/compile_windows.bat
+++ frontends/qt/compile_windows.bat
@@ -4,11 +4,11 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 cd build
 qmake ..\BitBox.pro
 nmake
-COPY "%VCToolsRedistDir%\x64\Microsoft.VC142.CRT\msvcp140.dll" windows\
-COPY "%VCToolsRedistDir%\x64\Microsoft.VC142.CRT\msvcp140_1.dll" windows\
-COPY "%VCToolsRedistDir%\x64\Microsoft.VC142.CRT\msvcp140_2.dll" windows\
-COPY "%VCToolsRedistDir%\x64\Microsoft.VC142.CRT\msvcp140_atomic_wait.dll" windows\
-COPY "%VCToolsRedistDir%\x64\Microsoft.VC142.CRT\msvcp140_codecvt_ids.dll" windows\
-COPY "%VCToolsRedistDir%\x64\Microsoft.VC142.CRT\vccorlib140.dll" windows\
-COPY "%VCToolsRedistDir%\x64\Microsoft.VC142.CRT\vcruntime140.dll" windows\
-COPY "%VCToolsRedistDir%\x64\Microsoft.VC142.CRT\vcruntime140_1.dll" windows\
+COPY "%VCToolsRedistDir%x64\Microsoft.VC142.CRT\msvcp140.dll" windows\
+COPY "%VCToolsRedistDir%x64\Microsoft.VC142.CRT\msvcp140_1.dll" windows\
+COPY "%VCToolsRedistDir%x64\Microsoft.VC142.CRT\msvcp140_2.dll" windows\
+COPY "%VCToolsRedistDir%x64\Microsoft.VC142.CRT\msvcp140_atomic_wait.dll" windows\
+COPY "%VCToolsRedistDir%x64\Microsoft.VC142.CRT\msvcp140_codecvt_ids.dll" windows\
+COPY "%VCToolsRedistDir%x64\Microsoft.VC142.CRT\vccorlib140.dll" windows\
+COPY "%VCToolsRedistDir%x64\Microsoft.VC142.CRT\vcruntime140.dll" windows\
+COPY "%VCToolsRedistDir%x64\Microsoft.VC142.CRT\vcruntime140_1.dll" windows\
